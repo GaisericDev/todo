68ba46e8d2b41f17d8d49e5fbe0ec19138a18362
Gankerjack <sebastiaancrisan@gmail.com>
2022-02-09 12:59:55 +0100

    rm junk

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,11 @@
 <template>
-  <hello-world />
+  <div class="home"><h1>Homepage</h1></div>
 </template>
 
 <script lang="ts">
-  import Vue from 'vue'
-  import HelloWorld from '../components/HelloWorld.vue'
+import Vue from "vue";
 
-  export default Vue.extend({
-    name: 'Home',
-
-    components: {
-      HelloWorld,
-    },
-  })
+export default Vue.extend({
+  name: "Home",
+});
 </script>
