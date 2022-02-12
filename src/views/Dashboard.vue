9469202e7536bfa53cd8d1eb78659cb992ae0524
Gaiseric <sebastiaancrisan@gmail.com>
2022-02-12 18:44:07 +0100

    apply grid system

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -4,7 +4,26 @@
       Dashboard
     </h1>
     <v-container class="my-5">
-      content
+      <v-card flat class="px-3">
+        <v-row wrap>
+          <v-col cols="12" md="6">
+            <div class="caption grey--text">Project title</div>
+            <div>Create a new website</div>
+          </v-col>
+          <v-col cols="12" xs="6" sm="4" md="2">
+            <div class="caption grey--text">Person</div>
+            <div>Gaiseric</div>
+          </v-col>
+          <v-col cols="12" xs="6" sm="4" md="2">
+            <div class="caption grey--text">Due by</div>
+            <div>1st March 2022</div>
+          </v-col>
+          <v-col cols="12" xs="2" sm="4" md="2">
+            <div class="caption grey--text">Status</div>
+            <div>Ongoing</div>
+          </v-col>
+        </v-row>
+      </v-card>
     </v-container>
   </div>
 </template>
