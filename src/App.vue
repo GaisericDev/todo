60cc724bed12ee8a28bea56ab1944a527f89af23
Gaiseric <sebastiaancrisan@gmail.com>
2022-02-14 19:33:22 +0100

    cleanup little bit

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,6 @@
 </template>
 
 <script lang="ts">
-//https://www.youtube.com/watch?v=BIuQfQcptIU&list=PL4cUxeGkcC9g0MQZfHwKcuB0Yswgb3gA5&index=4
 import Vue from "vue";
 import Navbar from "./components/Navbar.vue";
 export default Vue.extend({
